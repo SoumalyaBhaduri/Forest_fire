ae15dd441d1bbe77d48ea00143f59746015acd9c
Soumalya Bhaduri <56780120+sambhaduri@users.noreply.github.com>
2026-04-25 12:40:53 +0530

    APP version 2

--- application.py
+++ application.py
@@ -20,10 +20,23 @@ def index():
 @app.route("/predict", methods=['GET', 'POST'])
 def predict():
     if request.method=="POST":
-        pass
+        temperature = float(request.form.get('temperature'))
+        humidity = float(request.form.get('humidity'))
+        wind_speed = float(request.form.get('wind_speed'))
+        rainfall = float(request.form.get('rainfall'))
+        ffmc = float(request.form.get('ffmc'))
+        dmc = float(request.form.get('dmc'))
+        isi = float(request.form.get('isi'))
+        classes = float(request.form.get('classes'))
+
+        new_scaled_data = scaler.transform([[temperature, ffmc, dmc, isi, classes, humidity, wind_speed, rainfall]])
+
+        result = model.predict(new_scaled_data)
+
+        return render_template('home.html', results=round(result[0], 2 ))
     else:
         return render_template('home.html')
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
